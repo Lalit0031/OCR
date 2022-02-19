0f37f6fb3732cf9bf6468800d5c177a3e309b162
Lalit Kumar <69742783+Lalit0031@users.noreply.github.com>
2022-02-19 17:04:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import cv2
 import pytesseract
 import pyttsx3
 
-pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# you have to first download and install tesseract
+pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files\Tesseract-OCR\tesseract.exe' #please add location according to you.
 
 def ocr_img(img):
-    text = pytesseract.image_to_string(img)
-    return text
+    text = pytesseract.image_to_string(img)  # this will extract text from image to string
+    return text 
 
-img = cv2.imread(r'C:\Users\lalit\PycharmProjects\OCR\pic.jpeg')
+img = cv2.imread(r'C:\Users\lalit\PycharmProjects\OCR\pic.jpeg') # it reads the image
 
 print(ocr_img(img))
-engine = pyttsx3.init()
+engine = pyttsx3.init() # text to speech
 engine.say(ocr_img(img))
 
 engine.runAndWait()
